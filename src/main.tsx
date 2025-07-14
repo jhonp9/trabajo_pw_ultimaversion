fffc576639daafef08e8c0c438481e34d2569959
unknown <ola@gmail.com>
2025-07-14 13:44:14 -0500

    subido

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from './components/Auth/AuthContext';
 import { AdminProvider } from './context/AdminContext';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <AdminProvider>
-          <App />
-      </AdminProvider>
-    </AuthProvider>
+    <BrowserRouter basename='/trabajo_pw_ultimaversion'>
+      <AuthProvider>
+        <AdminProvider>
+            <App />
+        </AdminProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Backend en puerto 3000
+        target: 'https://trabajo-pw-backend-otv4.onrender.com', // Backend en puerto 3000
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
+  base: '/trabajo_pw_ultimaversion', // Ruta base para GitHub Pages
 });
